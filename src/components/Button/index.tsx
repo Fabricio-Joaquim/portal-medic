9f6ae8c5b9932c6c6c6e947aa66677685c19614a
Fabricio Joaquim <fabriciojoaquim25@hotmail.com>
2023-08-26 18:45:00 -0300

    refactor: :recycle: component refactore, and create button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+
+}
+
+export const Button = ({ children, ...rest }: Props) => {
+    return (
+        <button
+            className='relative inline-block m-4 px-8 py-4 text-base text-white ease-out duration-200 shadow-sm rounded-xl bg-slate-600
+hover:text-zinc-600 hover:bg-slate-100 hover:border-slate-700 hover:border-2 hover:shadow-2xl
+'
+            {...rest}>
+            {children}
+        </button>
+    )
+}
--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,14 +1,17 @@
+/* eslint-disable */
 import { FieldValues, UseControllerProps, useController } from "react-hook-form";
-
-type Props = {
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
     name: string;
     label: string;
-    control: UseControllerProps<FieldValues>['control'];
+    placeholder: string;
+    //@ts-ignore
+    control: UseControllerProps<FieldValues>[unknown];
+    Icon?: string;
 };
 
-export const InputText = ({ name: nameInput, label, control }: Props) => {
+export const InputText = ({ name: nameInput, label, control, placeholder, Icon, ...rest }: Props) => {
     const {
-        field: { ref, name, onBlur, onChange, value },
+        field: inputProps,
         fieldState: { invalid, error },
     } = useController({
         name: nameInput,
@@ -16,34 +19,17 @@ export const InputText = ({ name: nameInput, label, control }: Props) => {
         defaultValue: "",
     });
 
-/* 
-.input {
- border: none;
- padding: 1rem;
- border-radius: 1rem;
- background: #e8e8e8;
- box-shadow: 20px 20px 60px #c5c5c5,
-		-20px -20px 60px #ffffff;
- transition: 0.3s;
-}
-
-.input:focus {
- outline-color: #e8e8e8;
- background: #e8e8e8;
- box-shadow: inset 20px 20px 60px #c5c5c5,
-		inset -20px -20px 60px #ffffff;
- transition: 0.3s;
-}
-*/
-
     return (
-        <div>
-            <label htmlFor={name} >{label}</label>
-            <input className="border-none p-2 rounded-lg bg-gray-200 border-x-2 focus:outline-amber-300 duration-300 shadow-md" 
-            id={name} onChange={onChange} onBlur={onBlur} value={value} ref={ref} />
-            {invalid && <span>{error?.message}</span>}
+        <div  className="mb-4">
+            <label htmlFor={inputProps.name} className="block mb-2 text-sm font-medium text-black dark:text-white">{label}</label>
+            <div className="relative">
+                <div className={`absolute inset-y-0 left-0 flex items-center ${Icon && "pl-3.5"} pointer-events-none`}>
+                    {Icon && <Icon/>}
+                </div>
+                <input id={inputProps.name} className={`"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ${Icon && "pl-10"} p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"`} placeholder={placeholder} {...inputProps} {...rest} />
+            </div>
+            {invalid && <span className="text-red-600 font-normal text-xs">{error?.message}</span>}
         </div>
     );
 }
-
 
