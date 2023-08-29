73108c31721eab8f84576ab9152205372eaa9b10
Fabricio Joaquim <fabriciojoaquim25@hotmail.com>
2023-08-29 14:12:48 -0300

    refactor: :recycle: refactor register-medications

---
+++ src/helpers/objectToArrayLabelIsValue.ts
@@ -0,0 +1,13 @@
+export const objectToArrayLabelIsValue = (object: any) => {
+
+    if (typeof object !== 'object') {
+        return object;
+    }
+
+    const array = [];
+    for (const key in object) {
+        if (object[key])
+            array.push(key);
+    }
+    return array;
+}
--- src/pages/RegisterMedication/viewModel/useRegisterMedication.ts
+++ src/pages/RegisterMedication/viewModel/useRegisterMedication.ts
@@ -1,52 +1,63 @@
 import { registerMedicationSchema } from "../schema/registerMedication.schema";
+import { objectToArrayLabelIsValue } from "@helpers/objectToArrayLabelIsValue";
 import { RegisterMedication } from "../../../interface/registerMedication";
 import { formatOptionsSelect } from "../../../helpers/formatOptionsSelect";
 import { ManufacturerService } from "../../../service/manufacturerService"
 import { optionsSelect } from "../../../interface/optionsSelect";
 import { MedicationService } from "@services/medicationService";
-import { objectToArray } from "@helpers/objectToArray";
+import { useLoading } from "../../../hooks/useLoading";
+import { RouterEnum } from "../../../Enum/routerEnum";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 export const useRegisterMedication = () => {
     const resolverMemoSchemaLogin = useMemo(() => registerMedicationSchema, []);
     const [manufacture, setManufacture] = useState<optionsSelect[]>([] as optionsSelect[]);
+    const { setLoadingAction } = useLoading()
     const navigate = useNavigate();
 
-
-    const { control, handleSubmit, formState: { errors }, watch, register } = useForm<RegisterMedication>({
-        mode: "onBlur",
+    const { control, handleSubmit, formState: { errors } } = useForm<RegisterMedication>({
+        mode: "onChange",
         resolver: yupResolver(resolverMemoSchemaLogin),
     });
 
-    useEffect(() => {
+    const searchManufacture = async () => {
+        setLoadingAction(true)
         ManufacturerService.getManufacturers().then(({ data }) => {
-            const format = formatOptionsSelect(data, 'name', 'name');
-            setManufacture(format);
-        });
-    }, [])
+            const formatResponseManufacture = formatOptionsSelect(data, 'name', 'name');
+            setManufacture(formatResponseManufacture);
+        })
+            .catch(() => toast.error('Failed to load manufacturers'))
+            .finally(() => setLoadingAction(false))
+    }
 
     const onSubmit = (data: RegisterMedication) => {
-        const manufactoreFormated = objectToArray(data.manufacturers)
+        setLoadingAction(true)
+        const manufactoreFormated = objectToArrayLabelIsValue(data.manufacturers)
         const dataFormated = {
             ...data,
             manufacturers: manufactoreFormated
         }
 
         MedicationService.postMedications(dataFormated).then(() => {
-            navigate(-1)
-        })
+            toast.success('Medication registered successfully')
+            navigate(RouterEnum.HOME)
+        }).catch(() => toast.error('Failed to register medication'))
+            .finally(() => setLoadingAction(false))
     }
+
+    useEffect(() => {
+        searchManufacture()
+    }, [])
+
 
     return {
         control,
         onSubmit: handleSubmit(onSubmit),
         manufactureOptions: manufacture,
-        errors,
-        watch,
-        register
+        errors
     };
-
 }
